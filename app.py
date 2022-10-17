e1ead70eba2475b6d181827c12a9911a73a002eb
developer <mousesnake9@gmail.com>
2022-10-17 08:52:24 -0700

    second, endpoint

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return "hello, this is our first flask website"
+# @app.route('/home/<name>')
+# def home(name):
+#     return "hello," + name
 
+
+def about():
+    return "This is about Page"
+
+
+app.add_url_rule("/about", "about", about)
 
 if __name__ == '__main__':
     app.run(debug=True)
